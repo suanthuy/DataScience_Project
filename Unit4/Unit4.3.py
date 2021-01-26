dc32a4a64590260f4bfb6d987bbbc94d718819d9
suanthuy <77391220+suanthuy@users.noreply.github.com>
2021-01-26 10:32:43 +0700

    update unit4.3

--- Unit4/Unit4.3.py
+++ Unit4/Unit4.3.py
@@ -33,7 +33,18 @@ for s in students:
 
 #sum of num_of_exam_taken = len(students)
 
+import matplotlib.pyplot as plt
 
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = '0', '1', '2', '3','4','5','6','7','8','9','10','11'
+sizes = [0, 80, 122, 2598, 4333, 318, 2730, 64261, 0, 0, 0, 1]
+explode = (0, 0, 0, 0,0,0,0.1,0,0,0,0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()
 
 
 
